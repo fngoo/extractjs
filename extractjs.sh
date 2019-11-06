37e7be05aa01ffbaa131cc73d46f6d821d9fc99e
fngoo <38164287+fngoo@users.noreply.github.com>
2019-11-06 20:56:41 +0800

    Update extractjs.sh

--- extractjs.sh
+++ extractjs.sh
@@ -77,7 +77,7 @@ rm -r EyeWitness
 cd /root/script/4_getjs
 git clone https://github.com/FortyNorthSecurity/EyeWitness
 cd EyeWitness/setup ; bash setup.sh ; bash setup.sh ; pip3 install --upgrade pyasn1-modules
-mkdir $output/3_endpoint_js
+mkdir $output/endpoint_js
 cd /root/script/4_getjs/EyeWitness
 python3 EyeWitness.py -f $output/3_endpoint_JS.txt --web --no-prompt -d $output/endpoint_js
 
